b1e16337b30995db908c9ac31df084b88146b263
vidhya.m <vidhyam.stackup@gmail.com>
2024-12-06 12:03:43 +0530

    the full changes

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
+
 const button = document.getElementById('button');
+const close = document.getElementById('close-btn');
+const form = document.getElementById('container');
 
-button.addEventListener('click',function(){
+// hide or show the form  when the button is clicked
+button.addEventListener('click', function () {
+  if (form.style.display === 'none' || form.style.display === '') {
+    form.style.display = 'block'; // Show the form
+  } else {
+    form.style.display = 'none'; // Hide the form
+  }
+});
 
-   const form = document.getElementById('container');
-   if(form.style.display === 'none' || form.style.display === '' )
-    form.style.display = 'block';
-  else
-  form.style.display = 'none'
-})
+// Hide the form when the close button is clicked
+close.addEventListener('click', function () {
+  form.style.display = 'none';
+});
